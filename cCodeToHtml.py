161e975a09dc38d790600cad1037cb9a52880f21
grant farnsworth <gzp2307@wmich.edu>
2018-02-25 16:51:22 -0500

    finished body of examples

--- cCodeToHtml.py
+++ cCodeToHtml.py
@@ -1,20 +1,20 @@
 import re
-fileC = open("color.c","r")
+fileC = open("spectrum.c","r")
 stringC = fileC.read()
 fileC.close()
 
 stringC = re.sub("&","&amp;",stringC);
 stringC = re.sub("<","&lt;",stringC);
 stringC = re.sub(">","&gt;",stringC);
+stringC = re.sub("(.*)\n","<div> \\1 </div>\n",stringC)
 stringC = re.sub("\n","<br>\n",stringC);
-#stringC = re.sub("(.*)\n","<div class=\"codeLine\"> \\1 </div>\n",stringC)
 stringC = re.sub("(?<!class=)(?!\">)(\".*\")","<span class=\"string\">\\1</span>",stringC)
 stringC = re.sub("([\n\s\*\(\{])(int|double|char|FILE)([\n\s\)])","\\1<span class=\"declare\">\\2</span>\\3",stringC)
 stringC = re.sub("([\n\s\*\(\{\)])(for|if|else|fprintf|printf|sizeof|(?:seabreeze_[a-zA-Z0-9_]*[a-zA-Z0-9]))([\n\s\(])","\\1<span class=\"reserved\">\\2</span>\\3",stringC)
 print(stringC)
 
 
-fileHTML = open("color.html","w");
+fileHTML = open("spectrum.html","w");
 
-fileHTML.write("<meta  charset=\"utf-8\"><link rel=\"stylesheet\" href=\"css/code.css\">\n<code>"+stringC+"</code>");
+fileHTML.write(stringC);
 fileHTML.close();
--- js/panel.js
+++ js/panel.js
@@ -1,5 +1,17 @@
+var wrapStyle = ["nowrap","normal"];
+var wrapSelect1;
+var wrapSelect2
 window.onload= function() {
-    $("#example1Head").click(function() {
-    $("#example1Body").fadeToggle("fast");
-});
+    wrapSelect1 = 0;
+    wrapSelect2 = 0;
+    $("#example1Collapse").click(function() {
+    $("#example1Body").fadeToggle("fast");});
+    $("#example2Collapse").click(function() {
+    $("#example2Body").fadeToggle("fast");});
+    $("#example1Wrap").click(function() {
+    $("#example1Body").css("white-space",wrapStyle[wrapSelect1]);
+    wrapSelect1 = (wrapSelect1+1)%2});
+    $("#example2Wrap").click(function() {
+    $("#example2Body").css("white-space",wrapStyle[wrapSelect2]);
+    wrapSelect2 = (wrapSelect2+1)%2});
 }
