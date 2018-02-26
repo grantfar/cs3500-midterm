8db2e6f972619806d535d18f5e8e44854a1ee350
James Eckler <jameseckler1994@yahoo.com>
2018-02-26 14:21:35 -0500

    Changed wrapping/collapse buttons
    Widths are set in code.css so the buttons don't change width based on content.
    Wrap buttons also display if text wrapping is currently on or off.

--- js/panel.js
+++ js/panel.js
@@ -17,10 +17,16 @@ window.onload= function() {
         $("#example2Body").fadeToggle("fast");
     });
     $("#example1Wrap").click(function() {
+        $(this).text(function(i, text) {
+            return text === "Text Wrap: [ON]" ? "Text Wrap: [OFF]" : "Text Wrap: [ON]";
+        });
         $("#example1Body").css("white-space",wrapStyle[wrapSelect1]);
         wrapSelect1 = (wrapSelect1+1)%2;
     });
     $("#example2Wrap").click(function() {
+        $(this).text(function(i, text) {
+            return text === "Text Wrap: [ON]" ? "Text Wrap: [OFF]" : "Text Wrap: [ON]";
+        });
         $("#example2Body").css("white-space",wrapStyle[wrapSelect2]);
         wrapSelect2 = (wrapSelect2+1)%2;
     });
