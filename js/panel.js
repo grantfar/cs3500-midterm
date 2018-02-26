0d1ee405c3e0a1d9bd87f0fe29964723f021fac6
James Eckler <jameseckler1994@yahoo.com>
2018-02-26 12:54:09 -0500

    Added function to toggle collapse button text when clicked

--- js/panel.js
+++ js/panel.js
@@ -5,13 +5,23 @@ window.onload= function() {
     wrapSelect1 = 0;
     wrapSelect2 = 0;
     $("#example1Collapse").click(function() {
-    $("#example1Body").fadeToggle("fast");});
+        $(this).text(function(i, text) {
+            return text === "Collapse" ? "Expand" : "Collapse";
+        });
+        $("#example1Body").fadeToggle("fast");
+    });
     $("#example2Collapse").click(function() {
-    $("#example2Body").fadeToggle("fast");});
+        $(this).text(function(i, text) {
+            return text === "Collapse" ? "Expand" : "Collapse";
+        });
+        $("#example2Body").fadeToggle("fast");
+    });
     $("#example1Wrap").click(function() {
-    $("#example1Body").css("white-space",wrapStyle[wrapSelect1]);
-    wrapSelect1 = (wrapSelect1+1)%2});
+        $("#example1Body").css("white-space",wrapStyle[wrapSelect1]);
+        wrapSelect1 = (wrapSelect1+1)%2;
+    });
     $("#example2Wrap").click(function() {
-    $("#example2Body").css("white-space",wrapStyle[wrapSelect2]);
-    wrapSelect2 = (wrapSelect2+1)%2});
+        $("#example2Body").css("white-space",wrapStyle[wrapSelect2]);
+        wrapSelect2 = (wrapSelect2+1)%2;
+    });
 }
